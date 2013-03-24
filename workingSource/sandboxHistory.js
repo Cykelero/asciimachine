7861825d3aa8bd7268d35db0da1e2b0a39cff53c
Nathan Manceaux-Panot <cykelero@gmail.com>
2013-03-24 14:27:58 +0100

    Added a few machines to sandboxHistory

---
+++ workingSource/sandboxHistory.js
@@ -0,0 +1,62 @@
+
+      |
+|     |
+| HH--|-H
+H |@  H H
+H-|#-@H H-H
+XXXXXXXXXXX
+
+
+  #
+  @
+#-%>--H
+  @
+  #
+
+
+
+#%>H
+   H
+#@-%>
+    H
+
+
+ #H%>H%>H
+   H    H
+   HHH@HH
+  
+ 
+ 
+ 
+  #
+  %>H%>HH#
+ HH     H
+ H------H
+
+  #
+  %>H%>HH#
+  H     H
+  H-----H
+  
+   #
+ %>H%>HH#
+ @     H
+ HHHHHHH
+  
+         
+    #%>%>H
+     H   H
+     H   H
+#HH@HHHHHHHH
+
+
+#--H--H-----%>--H
+ H H  @--H  |   |
+   @  |   H-H   |
+   H--|---H
+      |
+
+
+@---H
+#---%>--
+@---H
