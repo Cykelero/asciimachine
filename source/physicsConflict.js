aa3d501a953684ca2c1bcb33927637abc0766df1
Nathan Manceaux-Panot <cykelero@gmail.com>
2013-04-10 22:46:06 +0200

    Conflicts are now always resolved from the winner's point of view if possible

--- source/physicsConflict.js
+++ source/physicsConflict.js
@@ -17,7 +17,7 @@ common.constructor = function(entityA, entityB, axis) {
 		axis: false
 	});
 	
-	exposed.resolve = function() {
+	exposed.resolve = function(preventDelegation) {
 		var forceA = internal.entityA.velocities[internal.axis],
 			forceB = internal.entityB.velocities[internal.axis];
 		
@@ -29,7 +29,19 @@ common.constructor = function(entityA, entityB, axis) {
 			loserEntity = aWins ? entityB : entityA,
 			loserForce = aWins ? forceB : forceA;
 		
-		// Clamping loser force amount
+		// Delegate resolution?
+		if (!preventDelegation) {
+			// Winner first
+			if (winnerEntity == internal.entityA) {
+				var conflict = winnerEntity.findConflictWith(loserEntity);
+				if (conflict) {
+					conflict.resolve(true);
+					return;
+				};
+			}
+		}
+		
+		// Didn't delegate: Clamping loser force amount
 		var direction = (winnerForce.amount != 0) ?
 			(winnerForce.amount > 0 ? 1 : -1)
 			: (loserForce.amount > 0 ? -1 : 1);
