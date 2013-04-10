1ace61ae04f0ba916c710c0486d494200b7165de
Nathan Manceaux-Panot <cykelero@gmail.com>
2013-04-10 22:38:27 +0200

    Added solid.findConflictWith

--- source/entityDefinitions/attributes/solid.js
+++ source/entityDefinitions/attributes/solid.js
@@ -63,65 +63,69 @@ MachineEntityTypesAggregator.defineAttribute("solid", function(attr, types) {
 			// // Providing info
 			
 			exposed.$findConflicts = function() {
-				var conflicts = [],
-					xSpeed = exposed.velocities[0].amount,
-					ySpeed = exposed.velocities[1].amount;
+				var conflicts = [];
 				
 				internal.parent.getEntitiesWith("solid").forEach(function(other) {
-					if (other == self) return;
-					
-					var otherProjected = other.getProjectedPosition(),
-						selfProjected = exposed.getProjectedPosition();
-					
-					var strongestAxis = +(exposed.velocities[0].type >= exposed.velocities[1].type);
-					
-					// X movement
-					if (xSpeed) {
-						if (selfProjected.x == otherProjected.x && internal.cell.y == otherProjected.y) {
-							var selfYVelocityDirection = (exposed.velocities[1].amount/Math.abs(exposed.velocities[1].amount)),
-								otherYVelocityDirection = (other.velocities[1].amount/Math.abs(other.velocities[1].amount));
-							
-							if (selfProjected == otherProjected || selfYVelocityDirection != otherYVelocityDirection) {
-								conflicts.push(new PhysicsConflict(self, other, 0));
-							}
-							return;
-						}
-					}
-					
-					// Y movement
-					if (ySpeed) {
-						if (internal.cell.x == otherProjected.x && selfProjected.y == otherProjected.y) {
-							var selfXVelocityDirection = (exposed.velocities[0].amount/Math.abs(exposed.velocities[0].amount)),
-								otherXVelocityDirection = (other.velocities[0].amount/Math.abs(other.velocities[0].amount));
-							
-							if (selfProjected == otherProjected || selfXVelocityDirection != otherXVelocityDirection) {
-								conflicts.push(new PhysicsConflict(self, other, 1));
-							}
-							return;
-						}
-					}
-					
-					// Complete movement
-					if (xSpeed && ySpeed) {
-						if (selfProjected == otherProjected) {
-							conflicts.push(new PhysicsConflict(self, other, strongestAxis));
-							return;
-						}
-					}
-					
-					// Position swap
-					if (xSpeed || ySpeed) {
-						var selfTargetIsOtherPosition = selfProjected.x == other.cell.x && selfProjected.y == other.cell.y,
-							otherTargetIsSelfPosition = otherProjected.x == self.cell.x && otherProjected.y == self.cell.y;
-						if (selfTargetIsOtherPosition && otherTargetIsSelfPosition) {
-							conflicts.push(new PhysicsConflict(self, other, strongestAxis));
-							return;
-						}
-					}
-					
+					var conflict = exposed.findConflictWith(other);
+					if (conflict) conflicts.push(conflict);
 				});
 				
 				return conflicts;
+			};
+			
+			exposed.findConflictWith = function(other) {
+				if (other == self) return;
+				
+				var xSpeed = exposed.velocities[0].amount,
+					ySpeed = exposed.velocities[1].amount;
+				
+				var otherProjected = other.getProjectedPosition(),
+					selfProjected = exposed.getProjectedPosition();
+				
+				var strongestAxis = +(exposed.velocities[0].type >= exposed.velocities[1].type);
+				
+				// X movement
+				if (xSpeed) {
+					if (selfProjected.x == otherProjected.x && internal.cell.y == otherProjected.y) {
+						var selfYVelocityDirection = (exposed.velocities[1].amount/Math.abs(exposed.velocities[1].amount)),
+							otherYVelocityDirection = (other.velocities[1].amount/Math.abs(other.velocities[1].amount));
+						
+						if (selfProjected == otherProjected || selfYVelocityDirection != otherYVelocityDirection) {
+							return new PhysicsConflict(self, other, 0);
+						}
+					}
+				}
+				
+				// Y movement
+				if (ySpeed) {
+					if (internal.cell.x == otherProjected.x && selfProjected.y == otherProjected.y) {
+						var selfXVelocityDirection = (exposed.velocities[0].amount/Math.abs(exposed.velocities[0].amount)),
+							otherXVelocityDirection = (other.velocities[0].amount/Math.abs(other.velocities[0].amount));
+						
+						if (selfProjected == otherProjected || selfXVelocityDirection != otherXVelocityDirection) {
+							return new PhysicsConflict(self, other, 1);
+						}
+					}
+				}
+				
+				// Complete movement
+				if (xSpeed && ySpeed) {
+					if (selfProjected == otherProjected) {
+						return new PhysicsConflict(self, other, strongestAxis);
+					}
+				}
+				
+				// Position swap
+				if (xSpeed || ySpeed) {
+					var selfTargetIsOtherPosition = selfProjected.x == other.cell.x && selfProjected.y == other.cell.y,
+						otherTargetIsSelfPosition = otherProjected.x == self.cell.x && otherProjected.y == self.cell.y;
+					
+					if (selfTargetIsOtherPosition && otherTargetIsSelfPosition) {
+						return new PhysicsConflict(self, other, strongestAxis);
+					}
+				}
+				
+				return null;
 			};
 			
 			exposed.getProjectedPosition = function() {
