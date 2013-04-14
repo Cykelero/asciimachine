03f737d271777aa3c980587958fdb5c1770f8d4f
Nathan Manceaux-Panot <cykelero@gmail.com>
2013-04-14 21:55:08 +0200

    Added powerGate attribute, XOR now uses it

---
+++ source/entityDefinitions/attributes/powerGate.js
@@ -0,0 +1,50 @@
+// needs machineEntityTypesAggregator.js
+
+// needs entityDefinitions/attributes/solid.js
+// needs entityDefinitions/attributes/conductor.js
+// needs entityDefinitions/attributes/hasArrows.js
+
+MachineEntityTypesAggregator.defineAttribute("powerGate", function(attr, types) {
+	return [attr.fixed, attr.conductor, attr.hasArrows, function(common) {
+		common.constructor = function() {
+			var exposed = this.exposed,
+				internal = this.internal,
+				parent = this.parent,
+				self = exposed;
+			
+			internal.backgroundColor = [100, 0, 170];
+			
+			// Behavior
+			exposed.initializeRelationships = function() {
+				parent.exposed.initializeRelationships();
+				
+				internal.wiredDirections = Direction.all();
+				
+				internal.arrows.forEach(function(info) {
+					var wireType = Direction.isVertical(info.direction) ? types["|"] : types["-"];
+					
+					internal.parent.addEntity(new wireType(internal.parent, "", info.entity.cell));
+					
+					var directionIndex = internal.wiredDirections.indexOf(info.direction);
+					if (directionIndex > -1) internal.wiredDirections.splice(directionIndex, 1);
+				});
+			};
+			
+			// Power node
+			internal.initializeOutputs = function() {
+				internal.arrows.forEach(function(info) {
+					var direction = info.direction;
+					internal.getNeighborsFrom([direction]).forEach(function(info) {
+						internal.proposeConnection(info.entity, {direction: info.direction, kind: "contact"});
+					});
+				});
+			};
+			
+			exposed.sharePowerState = function() {
+			};
+			
+			internal.spreadPowerState = function() {
+			};
+		};
+	}];
+});
--- source/entityDefinitions/types/power/XOR.js
+++ source/entityDefinitions/types/power/XOR.js
@@ -1,45 +1,16 @@
 // needs machineEntityTypesAggregator.js
 
-// needs entityDefinitions/attributes/fixed.js
-// needs entityDefinitions/attributes/conductor.js
-// needs entityDefinitions/attributes/hasArrows.js
+// needs entityDefinitions/attributes/powerGate.js
 
 MachineEntityTypesAggregator.defineType("%", function(attr, types) {
-	return [attr.fixed, attr.conductor, attr.hasArrows, function(common) {
+	return [attr.powerGate, function(common) {
 		common.constructor = function() {
 			var exposed = this.exposed,
 				internal = this.internal,
 				parent = this.parent,
 				self = exposed;
 			
-			internal.backgroundColor = [100, 0, 170];
-			
-			// Behavior
-			exposed.initializeRelationships = function() {
-				parent.exposed.initializeRelationships();
-				
-				internal.wiredDirections = Direction.all();
-				
-				internal.arrows.forEach(function(info) {
-					var wireType = Direction.isVertical(info.direction) ? types["|"] : types["-"];
-					
-					internal.parent.addEntity(new wireType(internal.parent, "", info.entity.cell));
-					
-					var directionIndex = internal.wiredDirections.indexOf(info.direction);
-					if (directionIndex > -1) internal.wiredDirections.splice(directionIndex, 1);
-				});
-			};
-			
 			// Power node
-			internal.initializeOutputs = function() {
-				internal.arrows.forEach(function(info) {
-					var direction = info.direction;
-					internal.getNeighborsFrom([direction]).forEach(function(info) {
-						internal.proposeConnection(info.entity, {direction: info.direction, kind: "contact"});
-					});
-				});
-			};
-			
 			internal.refreshPowerState = function() {
 				var powerCount = internal.powerState.getPowerCount(),
 					unstableCount = internal.powerState.getUnstableCount();
@@ -48,11 +19,6 @@ MachineEntityTypesAggregator.defineType("%", function(attr, types) {
 				
 				var outputsPower = (powerCount == 1);
 				internal.powerState.setAllOutputs(outputsPower);
-			};
-			
-			exposed.sharePowerState = function() {
-			};
-			internal.spreadPowerState = function() {
 			};
 		};
 		
