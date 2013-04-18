e44a2eae56d6b25fd2ed895e1c6c66c8162772ed
Nathan Manceaux-Panot <cykelero@gmail.com>
2013-04-18 21:31:04 +0200

    Added water
    Water actually is a FLUID. I kid you not.

---
+++ source/entityDefinitions/types/mobile/water.js
@@ -0,0 +1,21 @@
+// needs machineEntityTypesAggregator.js
+
+// needs entityDefinitions/attributes/fluid.js
+// needs entityDefinitions/attributes/gravity.js
+// needs entityDefinitions/attributes/conductor.js
+
+MachineEntityTypesAggregator.defineType("~", function(attr, types) {
+	return [attr.fluid, attr.gravity, attr.conductor, function(common) {
+		common.constructor = function() {
+			var exposed = this.exposed,
+				internal = this.internal,
+				parent = this.parent,
+				self = exposed;
+			
+			internal.color = [255, 255, 255];
+			internal.backgroundColor = [40, 90, 150];
+		};
+		
+		common.internal.name = "water";
+	}];
+});
