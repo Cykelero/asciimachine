7e6511e3f8db72336e82fbb5a49686bdd23d0762
Nathan Manceaux-Panot <cykelero@gmail.com>
2013-05-25 22:44:06 +0200

    Fixed entities do not collide with fixed entities anymore

--- source/entityDefinitions/attributes/fixed.js
+++ source/entityDefinitions/attributes/fixed.js
@@ -12,6 +12,11 @@ MachineEntityTypesAggregator.defineAttribute("fixed", function(attr, types) {
 			
 			internal.color = [0, 0, 0];
 			internal.backgroundColor = [255, 255, 255];
+			
+			exposed.doesCollideWith = function(entity) {
+				return parent.exposed.doesCollideWith(entity)
+					&& !entity.has("fixed");
+			}
 		};
 		
 		common.internal.weight = "static";
